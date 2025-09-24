5c3d3624a777ee44951d26771c4fbf5a09248e64
alex blade <aalexblade@gmail.com>
2025-09-24 21:17:47 +0300

    fix vete

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Chef-Claude-test/' 
+  base: '/Chef-Claude-test/', 
+  publicDir: 'public' 
 })
